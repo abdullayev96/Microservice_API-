34b341d96a6c338df0572b2157b3fcb6329a947f
Burhon Abdullaev <abdullaev_b@turonbank.uz>
2025-06-10 16:08:02 +0500

    API point o'zgardi

--- vote/votes/serializers.py
+++ vote/votes/serializers.py
@@ -12,19 +12,11 @@ class VoteSerializer(serializers.ModelSerializer):
 
     def get_question_data(self, obj):
         try:
-            response = requests.get(f'http://127.0.0.1:8002/question/{obj.question_id}/')
+            response = requests.get(f'http://127.0.0.1:8002/api/question/{obj.question_id}/')
             print(response.json())
             if response.status_code == 200:
                 return response.json()
         except Exception:
             return {"error": "Question service unreachable"}
         return None
-
-
-
-#
-# class VoteSerializer(serializers.ModelSerializer):
-# 	class Meta:
-# 		model = Vote
-# 		fields = ('id', 'question_id', 'date_updated')
 
